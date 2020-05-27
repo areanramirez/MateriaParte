f40b270d5ee26e156a8edab7df71cb5b2fb06eff
areanramirez <66035542+areanramirez@users.noreply.github.com>
2020-05-27 18:50:29 -0500

    Create BDMateria.cpp

---
+++ BDMateria.cpp
@@ -0,0 +1,25 @@
+/*
+ * BDMateria.cpp
+ *
+ *  Created on: 26 may. 2020
+ *      Author: ylera
+ */
+
+#include "BDMateria.h"
+
+BDMateria::BDMateria() {
+	// TODO Auto-generated constructor stub
+
+}
+
+BDMateria::~BDMateria() {
+	// TODO Auto-generated destructor stub
+}
+
+int BDMateria::guardaMateria(Materia materia){
+	int res;
+	res = 1;
+	return res;
+
+}
+
