219b4cc82473fc481d9ff8177e34bd6d403ec2d3
areanramirez <66035542+areanramirez@users.noreply.github.com>
2020-05-27 18:44:36 -0500

    Create ControlEscolar.h

---
+++ ControlEscolar.h
@@ -0,0 +1,18 @@
+/*
+ * ControlEscolar.h
+ *
+ *  Created on: 27 may. 2020
+ *      Author: ylera
+ */
+
+#ifndef CONTROLESCOLAR_H_
+#define CONTROLESCOLAR_H_
+
+class ControlEscolar {
+public:
+	ControlEscolar();
+	virtual ~ControlEscolar();
+	void Menu();
+};
+
+#endif /* CONTROLESCOLAR_H_ */
